2da6ac40fddee7b9cdcb02c908e6dce8dd789934
M.Anugrah Putra.S <100083581+anugrahputra17@users.noreply.github.com>
2026-01-05 09:00:48 +0700

    Remove PDF action from DiagnosesRelationManager
    Removed PDF action from record actions in DiagnosesRelationManager.

--- app/Filament/Resources/Patients/RelationManagers/DiagnosesRelationManager.php
+++ app/Filament/Resources/Patients/RelationManagers/DiagnosesRelationManager.php
@@ -153,13 +153,6 @@ class DiagnosesRelationManager extends RelationManager
                 ->dateTime('d M Y H:i'),
         ])
         ->recordActions([
-
-            \Filament\Actions\Action::make('pdf')
-                ->label('PDF')
-                ->icon('heroicon-o-document-arrow-down')
-                ->url(fn ($record) => route('diagnosis.resume.pdf', $record))
-                ->openUrlInNewTab(),
-
             EditAction::make(),
             DeleteAction::make(),
         ])
